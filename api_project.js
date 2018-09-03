1132ae6f9296736cb6f462a33e6a456c50391287
Mahendra-Singh-Chauhan <m.chauhankkm@gmail.com>
2018-09-03 15:45:20 +0530

    apidoc

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Meeting planner",
+  "version": "0.0.1",
+  "description": "Api documentation for Meeting planner",
+  "title": "Meeting planner APIs",
+  "url": "http://localhost:3003",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-09-03T10:11:16.907Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
